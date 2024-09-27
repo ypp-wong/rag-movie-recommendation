2c1b6daadee081b926f3709d872a6b68e1937de0
Patrick Wong <p.wong@kigroup.de>
2024-09-27 17:48:14 +0200

    test

--- dummy.py
+++ dummy.py
@@ -1,14 +1,9 @@
-from fastapi import FastAPI
-from azure.identity import DefaultAzureCredential
-from azure.identity import get_bearer_token_provider
+# Set up the query for generating responses
+from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from openai import AzureOpenAI
-import os
 
-# storage = os.environ.get("STORAGE_ACCOUNT_NAME")
-
-credential = DefaultAzureCredential()
-token_provider = get_bearer_token_provider(credential, "https://cognitiveservices.azure.com/.default")
+# credential = AzureKeyCredential("MIwNtZsgbVVN7TmhEDhDGPcEvQOONB0ShUMtVgmefMAzSeCKg99U")
 openai_client = AzureOpenAI(
     api_version="2024-02-15-preview",
     azure_endpoint="https://open-ai-for-scaling.openai.azure.com/",
@@ -21,8 +16,7 @@ openai_client = AzureOpenAI(
 #     credential=credential
 # )
 
-app = FastAPI()
-
+# This prompt provides instructions to the model
 GROUNDED_PROMPT = """
 You are a friendly assistant that recommends hotels based on activities and amenities.
 Answer the query using only the sources provided below in a friendly and concise bulleted manner.
@@ -32,16 +26,18 @@ Do not generate answers that don't use the sources below.
 Query: {query}
 """
 
+# Query is the question being asked. It's sent to the search engine and the LLM.
+query = "Can you recommend a few hotels near the ocean with beach access and good views"
 
-@app.post('/rag')
-async def rag(query):
-    response = openai_client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": GROUNDED_PROMPT.format(query=query)
-            }
-        ],
-        model="gpt4o-for-scaling"
-    )
-    return {"answer": response.choices[0].message.content}
+# Set up the search results and the chat thread.
+# Retrieve the selected fields from the search index related to the question.
+response = openai_client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": GROUNDED_PROMPT.format(query=query)
+        }
+    ],
+    model="gpt4o-for-scaling"
+)
+print(response.choices[0].message.content)
